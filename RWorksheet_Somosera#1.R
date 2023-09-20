bb5438c32f7e838cc3edc074eef9ce66080686fc
Joshua Somosera <joshua.somosera@students.isatu.edu.ph>
2023-09-20 05:56:30 +0000

    Worksheet_Somosera#1

---
+++ RWorksheet_Somosera#1.R
@@ -0,0 +1,52 @@
+age <-c(34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 27, 22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 42, 53, 41,51, 35, 24, 33, 41)
+age
+
+#1.
+length(age)
+
+#a. How many data points?
+#There are 34 data points 
+
+#2. Find the reciprocal
+
+
+
+#3. Assign 
+new_age <- c(age,0,age)
+
+#What happen to the new_age?
+#It displayed two sets of "age" between 0
+
+#4. sort 
+sort(age)
+
+#5 Maximum and Minimum 
+
+min(age)
+max(age)
+
+#6
+data <- c(2.4, 2.8, 2.1, 2.5, 2.4, 2.2, 2.5, 2.3, 2.5, 2.3, 2.4, 2.7)
+
+length (data)
+#a. How many data points?
+#There are 12 data points
+
+#7
+new <-data*2
+#The Data doubled its value
+
+#8.1
+seq(1:100)
+
+#8.2
+seq(20,60)
+
+#8.3
+mean(20:60)
+
+#8.4  
+sum(51,91)
+
+#8.5
+seq(1:1000) 
