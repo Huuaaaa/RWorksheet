90afb4255d36aafe28d02e5b9b99d6fd925dfd3f
Joshua Somosera <joshua.somosera@students.isatu.edu.ph>
2023-12-12 16:16:17 +0000

    (Update) Worksheet#6

--- Worksheet#6/RWorksheet_6.R
+++ Worksheet#6/RWorksheet_6.R
@@ -31,3 +31,73 @@ print("Original Data:")
 print(fertilizer_levels)
 print("Ordered Factor:")
 print(ordered_fertilizer)
+
+
+#3 You can use a factor to represent the levels undertaken by 10 subjects.
+
+ExrLvl <- c("l", "n", "n", "i", "l", "l", "n", "n", "i", "l")
+
+ExrFact <- factor(ExrLvl, levels = c("n", "l", "i"), labels = c("none", "light", "intense"))
+
+print(ExrFact)
+
+
+#4
+state <- c("tas", "sa", "qld", "nsw", "nsw", "nt", "wa", "wa", "qld",
+           "vic", "nsw", "vic", "qld", "qld", "sa", "tas", "sa", "nt",
+           "wa", "vic", "qld", "nsw", "nsw", "wa", "sa", "act", "nsw",
+           "vic", "vic", "act")
+#a. 
+StateFact<- factor(state)
+print(StateFact)
+#The resu;lts provide the different "Levels" provided in the vector.
+
+#5.
+incomes <- c(60, 49, 40, 61, 64, 60, 59, 54,
+             62, 69, 70, 42, 56, 61, 61, 61, 58, 51, 48,
+             65, 49, 49, 41, 48, 52, 46, 59, 46, 58, 43)
+#a. Sample Mean
+
+incmeans <- tapply(incomes, StateFact, mean)
+print(incmeans)
+
+#b. The results show the mean for each state provided 
+
+#6.
+stdError <- function(x) sqrt(var(x)/length(x))
+incster <- tapply(incomes, statef, stdError)
+
+#a. The standard error is that the object 'statef' is not found as the vector was named as 'state'.
+
+
+#7.
+install.packages("titanic")
+library(titanic)
+
+data("titanic_train")
+titanic_data <- titanic_train
+
+
+Survived <- subset(titanic_data, Survived == 1)
+NotSurvived <- subset(titanic_data, Survived == 0)
+
+print(Survived)
+print(NotSurvived)
+
+#8
+install.packages("readr")
+library(readr)
+breastcancer_wisconsin <- read_csv("Worksheet#6/breastcancer_wisconsin.csv")
+View(breastcancer_wisconsin)
+
+#a. The dataset is all about the recorded data of people that have breastcancer and its characteristics.
+
+
+
+
+#9
+install.packages("AppliedPredictiveModeling")
+library("AppliedPredictiveModeling")
+view(abalone)
+head(abalone)
+summary(abalone)
